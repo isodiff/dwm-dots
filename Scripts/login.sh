8dd0afe54eda55d369da0da016dfa06273c39d33
Isodiff <bogdan.cimoch@outlook.com>
2022-06-16 23:25:16 +0200

    void changes

--- Scripts/login.sh
+++ Scripts/login.sh
@@ -1,28 +1,51 @@
 #!/usr/bin/env bash
 
 ## kitty welcome window
+(
 kitty -1 -T="Hey, Hi, Hello" --wait-for-single-instance-window-close -d=/home/twfl/ /home/twfl/Scripts/./welcomelol.sh &
-echo -e "$(date) started the kitty terminal window \n\n" &
+echo -e "$(date) started the kitty terminal window \n\n"
+wait
+echo -e "$(date) kitty terminal window stopped\n\n"
+) &
 
 ## power manager
+(
 xfce4-power-manager &
-echo -e "$(date) started the power power manager \n\n" &
-
-## kdeconnect connection to android
-kdeconnect-indicator &
-kdeconnect-cli --refresh -l &
-kdeconnect-cli -d "2a8dc3a0d999f67f" --ping-msg="Asus Void is now online!" &
-echo -e "$(date) started the kdeconnect system \n\n" &
+echo -e "$(date) started the power manager \n\n"
+wait
+echo -e "$(date) power manager stopped\n\n"
+) &
 
 ## wallpaper & xsetroot dwm bar
+(
 /home/twfl/Scripts/./wallpaper-switcher.sh &
-echo -e "$(date) started the wallapper script \n\n" &
+echo -e "$(date) started the wallapper script \n\n"
 /home/twfl/Scripts/dwm-bar/./dwm_bar.sh &
-echo -e "$(date) started the dwm_bar script \n\n" &
+echo -e "$(date) started the dwm_bar script \n\n"
+wait
+echo -e "$(date) dwm_bar and wallpaper scripts stopped \n\n"
+) &
 
 ## Audio servers and daemons
+(
 pipewire &
 pipewire-pulse &
-echo -e "$(date) started the audio server \n\n" &
+echo -e "$(date) started the audio server \n\n"
 playerctld daemon &
-echo -e "$(date) started the playerctl daemon \n\n" &
+echo -e "$(date) started the playerctl daemon \n\n"
+wait
+echo -e "$(date) audio servers and daemons stopped \n\n"
+) &
+
+wait
+
+## kdeconnect connection to android
+(
+kdeconnect-indicator &
+kdeconnect-cli --refresh -l &
+wait
+kdeconnect-cli -d "2a8dc3a0d999f67f" --ping-msg="Asus Void is now online\!" &
+echo -e "$(date) started the kdeconnect system \n\n"
+wait
+echo -e "$(date) kdeconnect system stopped \n\n"
+)
--- Scripts/wallpaper-switcher.sh
+++ Scripts/wallpaper-switcher.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-while $(feh --bg-fill --randomize ~/Pictures/* &)
+while $(feh --bg-fill --randomize ~/Pictures/Catppuccin/* &)
 do
 	sleep 60
 done &
