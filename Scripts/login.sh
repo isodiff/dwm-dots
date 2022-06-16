ed9b27e98a8dd8a6651cf1157d13fb08059a2947
Isodiff <bogdan.cimoch@outlook.com>
2022-06-16 21:24:49 +0200

    void changes

--- Scripts/login.sh
+++ Scripts/login.sh
@@ -1,52 +1,28 @@
-#!/bin/bash
-LOGDIR=$HOME"/Scripts/logs"
+#!/usr/bin/env bash
 
-# Dwm bar xsetroot
-(
-echo -e "\n\n"
-bash /home/twfl/Scripts/dwm-bar/dwm_bar.sh &
-echo -e "$(date) started the dwm_bar script \n\n" &
-) 2>&1 | tee -a $LOGDIR/scripts.log &
-
-# Start audio servers
-(
-sleep 2
-echo -e "\n\n"
-(pipewire) &
-(pipewire-pulse) &
-echo -e "$(date) started the audio server \n\n" &
-) 2>&1 | tee -a $LOGDIR/scripts.log &
-
-# Playerctl deamon for music status
-(
-echo -e "\n\n"
-playerctld daemon
-echo -e "$(date) started the playerctl daemon \n\n" &
-) 2>&1 | tee -a $LOGDIR/scripts.log &
-
-
-
-# Set a wallaper
-(
-sleep 2
-echo -e "\n\n"
-bash /home/twfl/Scripts/wallpaper-switcher.sh &
-echo -e "$(date) started the wallapper script \n\n" &
-) 2>&1 | tee -a $LOGDIR/scripts.log &
-
-
-# Open the kitty terminal
-(
-sleep 2
-echo -e "\n\n"
-kitty -1 -T="Hey, Hi, Hello" --wait-for-single-instance-window-close -d=/home/twfl/ bash /home/twfl/Scripts/welcomelol.sh &
+## kitty welcome window
+kitty -1 -T="Hey, Hi, Hello" --wait-for-single-instance-window-close -d=/home/twfl/ /home/twfl/Scripts/./welcomelol.sh &
 echo -e "$(date) started the kitty terminal window \n\n" &
-) 2>&1 | tee -a $LOGDIR/scripts.log &
 
-# Open Browser
-(
-sleep 2
-echo -e "\n\n"
-ungoogled-chromium &
-echo -e "$(date) started the browser \n\n" &
-) 2>&1 | tee -a $LOGDIR/scripts.log &
+## power manager
+xfce4-power-manager &
+echo -e "$(date) started the power power manager \n\n" &
+
+## kdeconnect connection to android
+kdeconnect-indicator &
+kdeconnect-cli --refresh -l &
+kdeconnect-cli -d "2a8dc3a0d999f67f" --ping-msg="Asus Void is now online!" &
+echo -e "$(date) started the kdeconnect system \n\n" &
+
+## wallpaper & xsetroot dwm bar
+/home/twfl/Scripts/./wallpaper-switcher.sh &
+echo -e "$(date) started the wallapper script \n\n" &
+/home/twfl/Scripts/dwm-bar/./dwm_bar.sh &
+echo -e "$(date) started the dwm_bar script \n\n" &
+
+## Audio servers and daemons
+pipewire &
+pipewire-pulse &
+echo -e "$(date) started the audio server \n\n" &
+playerctld daemon &
+echo -e "$(date) started the playerctl daemon \n\n" &
